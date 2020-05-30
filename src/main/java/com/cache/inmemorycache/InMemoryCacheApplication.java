f66b4d8bd96a62fa693e68b70a5a3114fdf29fb3
thusitha <thusitha@thusitha-Asus>
2020-05-30 13:20:38 +0530

    added

--- src/main/java/com/cache/inmemorycache/InMemoryCacheApplication.java
+++ src/main/java/com/cache/inmemorycache/InMemoryCacheApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwoLevelCacheApplication {
+public class InMemoryCacheApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwoLevelCacheApplication.class, args);
+		SpringApplication.run(InMemoryCacheApplication.class, args);
 	}
 
 }
