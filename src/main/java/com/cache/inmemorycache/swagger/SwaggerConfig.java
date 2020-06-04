26fed887ffeb6e999f7ad507aa6dbd79186338a7
thusitha <thusitha@thusitha-Asus>
2020-06-04 09:38:28 +0530

    added

--- src/main/java/com/cache/inmemorycache/swagger/SwaggerConfig.java
+++ src/main/java/com/cache/inmemorycache/swagger/SwaggerConfig.java
@@ -6,17 +6,18 @@ import static springfox.documentation.builders.PathSelectors.regex;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicate;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.BasicAuth;
-import springfox.documentation.service.Contact;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
@@ -27,6 +28,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	@Value("${version.number}")
+	private String version;
 
 	@Bean
 	public Docket api() {
@@ -35,30 +39,24 @@ public class SwaggerConfig {
 				.apis(RequestHandlerSelectors.any())
 				.paths(paths())
 				.build()
+				.apiInfo(apiInfo())
 				.securityContexts(Arrays.asList(actuatorSecurityContext()))
 	            .securitySchemes(Arrays.asList(basicAuthScheme()));
 	}	
 	 
-	/*
-	 * Paths to load and paths not to load
-	 */
+	
 	private Predicate<String> paths() {
 		return and(or( // Paths to Load
 				regex("/cache.*")));
 	}
 	
-    private ApiInfo metaData() {
-        ApiInfo apiInfo = new ApiInfo(
-
-                "Spring Boot REST API",
-                "Spring Boot REST API for Online Store",
-                "1.0",
-                "Terms of service",
-                new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licenses/LICENSE-2.0");
-        return apiInfo;
-    }
+	private ApiInfo apiInfo() {
+	    return new ApiInfoBuilder()
+	            .title("My API")
+	            .description("My API Documentation")
+	            .version(version)
+	            .build();
+	}
     
     private SecurityContext actuatorSecurityContext() {
         return SecurityContext.builder()
