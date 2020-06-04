833325c1a22175cbf69916ccc2b432783bbaaae5
thusitha <thusitha@thusitha-Asus>
2020-06-04 09:30:35 +0530

    added

--- src/main/java/com/cache/inmemorycache/swagger/SwaggerConfig.java
+++ src/main/java/com/cache/inmemorycache/swagger/SwaggerConfig.java
@@ -4,15 +4,23 @@ import static com.google.common.base.Predicates.and;
 import static com.google.common.base.Predicates.or;
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicate;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,9 +34,11 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(paths())
-				.build();
-	}
-
+				.build()
+				.securityContexts(Arrays.asList(actuatorSecurityContext()))
+	            .securitySchemes(Arrays.asList(basicAuthScheme()));
+	}	
+	 
 	/*
 	 * Paths to load and paths not to load
 	 */
@@ -48,5 +58,20 @@ public class SwaggerConfig {
                 "Apache License Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0");
         return apiInfo;
+    }
+    
+    private SecurityContext actuatorSecurityContext() {
+        return SecurityContext.builder()
+                .securityReferences(Arrays.asList(basicAuthReference()))
+                .forPaths(PathSelectors.ant("/cache/**"))
+                .build();
+    }
+
+    private SecurityScheme basicAuthScheme() {
+        return new BasicAuth("basicAuth");
+    }
+
+    private SecurityReference basicAuthReference() {
+        return new SecurityReference("basicAuth", new AuthorizationScope[0]);
     }
 }
